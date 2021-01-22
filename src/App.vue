89e84dfaf78dc5f1aff974258e84ee2d373dfa56
“Debora <debora.crescenzo@gmail.com>
2021-01-22 21:31:37 +0000

    basic form

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hello</h1>
+  <SignupForm/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import SignupForm from './components/SignupForm'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { SignupForm
+    
   }
 }
 </script>
@@ -22,5 +22,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body{
+  margin: 0;
+  background: #eeee;
 }
 </style>
